19c3f42a9d2efc752adb4a6c2efbaa5bfef3a2d0
PedroMuniz21 <91923226+PedroMuniz21@users.noreply.github.com>
2022-01-21 20:30:33 -0300

    Adicionando arquivos da Dh até aqui

---
+++ superHerois/index.js
@@ -0,0 +1,12 @@
+const herois = [
+    {
+        nome: "Batman",
+        poder: "Inteligencia"
+    },
+    {
+        nome:"SuperMan",
+        poder:"Super força"
+    }
+]
+
+module.exports = herois;
